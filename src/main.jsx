2638207b1f1078073e4d9b87201deb776c61c912
Siluetaz <ysmaelborquezz@gmail.com>
2022-11-16 15:35:56 -0300

    optimization

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
-import './sass/layouts/index.scss'
+import './sass/layouts/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
