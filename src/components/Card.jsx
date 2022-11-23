411f231f10bff05cca7c15cfd4786d38f1848e68
Siluetaz <ysmaelborquezz@gmail.com>
2022-11-23 14:48:29 -0300

    animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,12 @@ const Card = ({ project }) => {
   const imgUrl = new URL(project.imgDirection, import.meta.url).href
   return (
     <div className='card'>
-      <img className='card__image' src={imgUrl} alt="project-image" width="200px" height="100px" />
-      <p className='card__name'>{project.name}</p>
-      <ul className='card__technologies'>
-        {project.technologies.map((tech, i) => <li key={i}>{tech}</li>)}
-      </ul>
+      <div className='card__info'>
+        <img className='card__image' src={imgUrl} alt="project-image" width="200px" height="100px" />
+        <p className='card__name'>{project.name}</p>
+        <ul className='card__technologies'>
+          {project.technologies.map((tech, i) => <li key={i}>{tech}</li>)}
+        </ul></div>
       <div className='card__buttons'>
         <a className='button' href={project.siteUrl}>Ver Proyecto</a>
         <a className='button' href={project.codeUrl}>Ver código</a>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,15 +3,44 @@ import { Card } from './Card'
 import { Knowledge } from './Knowledge'
 import data from '../../data.json'
 import ScrollContainer from 'react-indiana-drag-scroll';
+import { useEffect, useState } from 'react';
 
 const Content = () => {
+
+  const [welChar, setWelChar] = useState('o')
+
+  useEffect(() => {
+    changeWelChar()
+  }, [welChar])
+
+  const changeWelChar = () => {
+    setTimeout(() => {
+      if (welChar === 'o') {
+        setWelChar('')
+        setTimeout(() => {
+          setWelChar('a')
+        }, 1000)
+      } else if (welChar === 'a') {
+        setWelChar('')
+        setTimeout(() => {
+          setWelChar('o')
+        }, 1000)
+      }
+    }, 2000)
+  }
   return (
     <main className='container'>
       <section className='welcome-side'>
-        <h1 className='welcome-side__title'>¡Bienvenido/a!<br></br> Soy <span className='under'>Ismael Borquez</span></h1>
+        <span className='welcome-side__wel-message'>{`Bienvenid${welChar}`}</span>
+        <h1 className='welcome-side__title'>Soy <span className='under'>Ismael Borquez</span></h1>
         <p className='welcome-side__presentation'>Analista programador, apasionado por el desarrollo y el adquirir nuevas habilidades y conocimientos.</p>
       </section>
       <hr className='line' />
+      <section>
+        <div>
+          
+        </div>
+      </section>
       <Knowledge />
       <section className='projects-side'>
         <h2 className='subtitle'>Proyectos</h2>
