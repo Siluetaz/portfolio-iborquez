e2d5dec8eee0e20a038edc552c324b877d6bcb18
Siluetaz <ysmaelborquezz@gmail.com>
2022-11-26 03:15:06 -0300

    animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { Footer } from './components/Footer'
 import { Content } from './components/Content'
 
 import './sass/layouts/App.scss'
+import { useEffect } from 'react'
 
 const App = () => {
+  window.onunload = function () {
+    window.scrollTo(0, 0);
+  }
   return (
     <>
-      <Header/>
-      <Content/>
-      <Footer/>
+      <Header />
+      <Content />
+      <Footer />
     </>
   )
 }
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,8 +3,8 @@ import { BsGithub, BsLinkedin } from "react-icons/bs";
 const Social = () => {
   return (
     <ul className='social'>
-      <li className='social__item'><a className='social__item__logo' href="https://github.com/Siluetaz" aria-label="My Github Page Link"><BsGithub className='social__item__logo' /></a></li>
-      <li className='social__item'><a className='social__item__logo' href="https://www.linkedin.com/in/ismael-borquez-ba%C3%B1ados-7b57a114b/" aria-label="My Linkedin Page Link"><BsLinkedin /></a></li>
+      <li className='social__item'><a className='social__item__logo' href="https://github.com/Siluetaz" target="_blank" aria-label="My Github Page Link"><BsGithub className='social__item__logo' /></a></li>
+      <li className='social__item'><a className='social__item__logo' href="https://www.linkedin.com/in/ismael-borquez-ba%C3%B1ados-7b57a114b/" target="_blank" aria-label="My Linkedin Page Link"><BsLinkedin /></a></li>
     </ul >
   )
 }
