bd505fce2e4597cb8ce1a81f166076e1075ff919
Siluetaz <ysmaelborquezz@gmail.com>
2022-11-26 17:40:52 -0300

    slader changed

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,33 +5,26 @@ import { Skill } from './Skill'
 import { Welcome } from './Welcome'
 import data from '../../data.json'
 import ScrollContainer from 'react-indiana-drag-scroll';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import '../../node_modules/pure-react-carousel/dist/react-carousel.es.css';
+import Glider from 'react-glider';
+import 'glider-js/glider.min.css'; 
 const Content = () => {
-
-
   return (
     <main className='container'>
       <Welcome />
       <section className='opacity'>
         <hr className='line' />
-        <CarouselProvider
-          naturalSlideWidth={80}
-          naturalSlideHeight={80}
-          totalSlides={2}
-          isPlaying={true}
-          lockOnWindowScroll={true}
-          className="views">
-          <Slider className='view'>
-            <Slide index={0}>
-              <Knowledge />
-            </Slide>
-            <Slide index={1}>
-              <Skill />
-            </Slide>
-          </Slider>
-          <ButtonBack className='navigation__circle'></ButtonBack>
-          <ButtonNext className='navigation__circle'></ButtonNext></CarouselProvider>
+        <Glider
+          draggable
+          hasDots
+          slidesToShow={1}
+          scrollLock
+          dragVelocity={3.8}
+          scrollLockDelay={210}
+          duration={0.2}
+        >
+          <div><Knowledge /></div>
+          <div><Skill /></div>
+        </Glider>
         <section className='projects-side'>
           <h2 className='subtitle'>Proyectos</h2>
           <ScrollContainer className="projects-side__container" hideScrollbars={false} horizontal={true}>
